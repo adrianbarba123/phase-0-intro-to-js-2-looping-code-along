561b29e4dab4832df3a15a9d3b60886a78f6a53b
Adrian Barba <adrianbarba123@gmail.com>
2022-11-15 21:00:37 -0800

    commit tree 07b06e42b7b53c55a0f480b100e75d700aa9e6a1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names){
+    const namesInput = [];
+    for(let i = 0; i < names.length; i++){
+        namesInput[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    }
+    return namesInput;
+};
+
+function countDown(x){
+    for (let i = 0; i < x+1; i++) {
+        console.log(i);
+    };
+};
